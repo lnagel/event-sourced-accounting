a704329417b7a963c41ca5198b6604696744193c
Lenno Nagel <lenno@nagel.ee>
2014-02-13 12:36:03 +0100

    Revert "Improve the code quality"
    This reverts commit 6d36b961bb8662bf8f31d4fe5f7120a44408fbfe.

--- app/models/esa/transaction.rb
+++ app/models/esa/transaction.rb
@@ -71,8 +71,9 @@ module ESA
     end
 
     def accounts_of_the_same_chart?
-      if self.accounts.pluck(:chart_id).compact.uniq.count != 1
-        errors[:base] << "Transaction must take place between accounts of the same Chart"
+      chart_ids = (self.credit_amounts + self.debit_amounts).map{|a| a.account.chart_id}
+      if chart_ids.compact.count != chart_ids.count or chart_ids.compact.uniq.count != 1
+        errors[:base] << "Transaction must take place between accounts of the same Chart " + chart_ids.to_s
       end
     end
 
