5af04a04264dc7abdd200fd4e832c70c7b62ae4e
Lenno Nagel <lenno@nagel.ee>
2014-02-13 17:07:41 +0100

    Add comparisons to DateFilter

--- app/models/esa/filters/date_filter.rb
+++ app/models/esa/filters/date_filter.rb
@@ -6,7 +6,11 @@ module ESA
 
         included do
           scope :between_dates, lambda { |date1,date2| joins(:transaction).where(esa_transactions: {time: date1.midnight..date2.end_of_day}) }
-          scope :with_date, lambda { |date| joins(:transaction).where(esa_transactions: {time: date.midnight..date.end_of_day}) }
+          scope :with_date,     lambda { |date| joins(:transaction).where(esa_transactions: {time: date.midnight..date.end_of_day}) }
+          scope :with_date_lt,  lambda { |date| joins(:transaction).where(ESA::Transaction.arel_table[:time].lt( date.midnight)) }
+          scope :with_date_gt,  lambda { |date| joins(:transaction).where(ESA::Transaction.arel_table[:time].gt( date.end_of_day)) }
+          scope :with_date_lte, lambda { |date| joins(:transaction).where(ESA::Transaction.arel_table[:time].lteq(date.end_of_day)) }
+          scope :with_date_gte, lambda { |date| joins(:transaction).where(ESA::Transaction.arel_table[:time].gteq(date.midnight)) }
         end
       end
 
@@ -15,7 +19,11 @@ module ESA
 
         included do
           scope :between_dates, lambda { |date1,date2| where(time: date1.midnight..date2.end_of_day) }
-          scope :with_date, lambda { |date| where(time: date.midnight..date.end_of_day) }
+          scope :with_date,     lambda { |date| where(time: date.midnight..date.end_of_day) }
+          scope :with_date_lt,  lambda { |date| where(arel_table[:time].lt( date.midnight)) }
+          scope :with_date_gt,  lambda { |date| where(arel_table[:time].gt( date.end_of_day)) }
+          scope :with_date_lte, lambda { |date| where(arel_table[:time].lteq(date.end_of_day)) }
+          scope :with_date_gte, lambda { |date| where(arel_table[:time].gteq(date.midnight)) }
         end
       end
     end
