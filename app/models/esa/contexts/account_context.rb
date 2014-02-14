abc407be8c4ed13956ed9fc901da934ec6d98554
Lenno Nagel <lenno@nagel.ee>
2014-02-14 09:05:57 +0100

    Unify style

--- app/models/esa/contexts/account_context.rb
+++ app/models/esa/contexts/account_context.rb
@@ -2,7 +2,7 @@ module ESA
   module Contexts
     class AccountContext < ESA::Context
       def initialize(account)
-        filter = lambda { |scoped| scoped.with_account(account) }
+        filter = lambda { |relation| relation.with_account(account) }
         super([filter])
       end
     end
--- app/models/esa/contexts/date_context.rb
+++ app/models/esa/contexts/date_context.rb
@@ -2,8 +2,8 @@ module ESA
   module Contexts
     class DateContext < ESA::Context
       def initialize(date)
-        date_filter = lambda { |scoped| scoped.with_date(date) }
-        super([date_filter])
+        filter = lambda { |relation| relation.with_date(date) }
+        super([filter])
       end
     end
   end
