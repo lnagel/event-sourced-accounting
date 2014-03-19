64d5061bffa01b5cfbd353c6ced2a06b6f34340d
Lenno Nagel <lenno@nagel.ee>
2014-03-19 15:26:26 +0100

    Accountable trait incldudes accessors for ruleset, chart and event creation functions

--- app/models/esa/traits/accountable.rb
+++ app/models/esa/traits/accountable.rb
@@ -7,6 +7,22 @@ module ESA
         has_many :esa_events,       :as => :accountable, :class_name => ESA::Event.extension_name(self),       :extend => ESA::Associations::EventsExtension
         has_many :esa_flags,        :as => :accountable, :class_name => ESA::Flag.extension_name(self),        :extend => ESA::Associations::FlagsExtension
         has_many :esa_transactions, :as => :accountable, :class_name => ESA::Transaction.extension_name(self), :extend => ESA::Associations::TransactionsExtension
+
+        def esa_ruleset
+          ESA::Ruleset.extension_instance(self)
+        end
+
+        def esa_chart
+          self.esa_ruleset.chart
+        end
+
+        def create_esa_events
+          self.esa_ruleset.create_unrecorded_events(self)
+        end
+
+        def create_esa_events!
+          self.esa_ruleset.create_unrecorded_events!(self)
+        end
       end
     end
   end
