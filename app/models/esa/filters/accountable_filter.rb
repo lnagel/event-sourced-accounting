f87fec2617223b0974cf9ca68d0629afb5848c40
Lenno Nagel <lenno@nagel.ee>
2014-03-26 12:46:50 +0100

    AccountableFilter optimization

--- app/models/esa/filters/accountable_filter.rb
+++ app/models/esa/filters/accountable_filter.rb
@@ -5,7 +5,10 @@ module ESA
         extend ActiveSupport::Concern
 
         included do
-          scope :with_accountable, lambda { |accountable,type| joins(:transaction).where(esa_transactions: {accountable_id: accountable, accountable_type: type}) }
+          scope :with_accountable, lambda { |accountable,type|
+            if accountable.is_a? ActiveRecord::Relation then accountable = accountable.pluck(accountable.primary_key) end
+            joins(:transaction).where(esa_transactions: {accountable_id: accountable, accountable_type: type})
+          }
           scope :with_accountable_type, lambda { |type| joins(:transaction).where(esa_transactions: {accountable_type: type}) }
         end
       end
@@ -14,7 +17,10 @@ module ESA
         extend ActiveSupport::Concern
 
         included do
-          scope :with_accountable, lambda { |accountable,type| where(accountable_id: accountable, accountable_type: type) }
+          scope :with_accountable, lambda { |accountable,type|
+            if accountable.is_a? ActiveRecord::Relation then accountable = accountable.pluck(accountable.primary_key) end
+            where(accountable_id: accountable, accountable_type: type)
+          }
           scope :with_accountable_type, lambda { |type| where(accountable_type: type) }
         end
       end
