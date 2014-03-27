e0a231506db47a1dd9e179f37584aeaf2c67d8a8
Lenno Nagel <lenno@nagel.ee>
2014-03-27 12:42:26 +0100

    CreatedAtContext for freshness checking

---
+++ app/models/esa/contexts/created_at_context.rb
@@ -0,0 +1,35 @@
+module ESA
+  module Contexts
+    class CreatedAtContext < ESA::Context
+      after_initialize :set_namespace
+      before_save :prevent_save
+
+      def created_at
+        @created_at
+      end
+
+      def created_at=(timespec)
+        @created_at = timespec
+        @filters = [lambda { |relation| relation.where(created_at: timespec) }]
+      end
+
+      def effective_path
+        if self.parent_id.blank?
+          []
+        else
+          self.parent.effective_path
+        end
+      end
+
+      protected
+
+      def set_namespace
+        self.namespace = "created_at"
+      end
+
+      def prevent_save
+        raise "#{self.type} objects are not intended to be persisted"
+      end
+    end
+  end
+end
