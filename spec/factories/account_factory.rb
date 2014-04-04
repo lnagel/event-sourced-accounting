525a28b6864a1de2e0f6d73b30cb6fdbdc27c7d9
Lenno Nagel <lenno@nagel.ee>
2014-04-04 14:52:23 +0200

    Add code

--- spec/factories/account_factory.rb
+++ spec/factories/account_factory.rb
@@ -1,35 +1,41 @@
 FactoryGirl.define do
   factory :account, :class => ESA::Account do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
   end
 
   factory :asset, :class => ESA::Accounts::Asset do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
   end
 
   factory :equity, :class => ESA::Accounts::Equity do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
   end
 
   factory :expense, :class => ESA::Accounts::Expense do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
   end
 
   factory :liability, :class => ESA::Accounts::Liability do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
   end
 
   factory :revenue, :class => ESA::Accounts::Revenue do |account|
+    account.code
     account.name
     account.contra false
     account.association :chart, :factory => :chart
@@ -37,5 +43,9 @@ FactoryGirl.define do
 
   sequence :name do |n|
     "Factory Name #{n}"
+  end
+
+  sequence :code do |n|
+    "#{n}"
   end
 end
