23967b51949d8c979781a073efb45557d70fc24b
Lenno Nagel <lenno@nagel.ee>
2014-04-06 17:19:24 +0200

    Fix tests

--- spec/factories/transaction_factory.rb
+++ spec/factories/transaction_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :transaction, :class => ESA::Transaction do |transaction|
     transaction.description 'factory description'
     factory :transaction_with_credit_and_debit, :class => ESA::Transaction do |transaction_cd|
-      transaction_cd.after_build do |t|
+      transaction_cd.after(:build) do |t|
         t.amounts << FactoryGirl.build(:credit_amount, :transaction => t)
         t.amounts << FactoryGirl.build(:debit_amount, :transaction => t)
       end
