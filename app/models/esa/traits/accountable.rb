862d311578322d2bf740c73df69edd9b717fb681
Lenno Nagel <lenno@nagel.ee>
2014-04-28 17:18:13 +0200

    Do not allow destruction of Accountables having Transactions

--- app/models/esa/traits/accountable.rb
+++ app/models/esa/traits/accountable.rb
@@ -8,12 +8,23 @@ module ESA
         has_many :esa_flags,        :as => :accountable, :class_name => ESA::Flag.extension_name(self),        :extend => ESA::Associations::FlagsExtension
         has_many :esa_transactions, :as => :accountable, :class_name => ESA::Transaction.extension_name(self), :extend => ESA::Associations::TransactionsExtension
 
+        before_destroy :destroy_accountable
+
         def esa_ruleset
           ESA::Ruleset.extension_instance(self)
         end
 
         def esa_chart
           self.esa_ruleset.chart
+        end
+
+        def destroy_accountable
+          if self.esa_transactions.blank?
+            self.esa_flags.delete_all
+            self.esa_events.delete_all
+          else
+            false
+          end
         end
       end
     end
