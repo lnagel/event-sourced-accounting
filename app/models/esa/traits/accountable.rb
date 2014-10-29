27b568dcb1c53b657863641aa82f33b34d68f304
Lenno Nagel <lenno@nagel.ee>
2014-10-29 09:48:25 +0200

    Revert "Remove esa_states entries when destroying the accountable"
    This reverts commit fff2221a267b2b22d6ab307a44d0f4acc654294c.

--- app/models/esa/traits/accountable.rb
+++ app/models/esa/traits/accountable.rb
@@ -35,7 +35,6 @@ module ESA
 
         def destroy_accountable
           if self.esa_transactions.blank?
-            self.esa_states.delete_all
             self.esa_flags.delete_all
             self.esa_events.delete_all
           else
